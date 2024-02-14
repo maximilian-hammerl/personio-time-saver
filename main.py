5d3f3401791878248cf7d90384841e78568e533c
Maximilian Hammerl <maximilian@hammerl.dev>
2024-02-14 21:07:17 +0100

    Logging

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import logging
 import time
 import configparser
@@ -9,9 +11,18 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.core.logger import set_logger
 from webdriver_manager.firefox import GeckoDriverManager
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger()
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+stream_handler.setFormatter(logging.Formatter(
+    fmt='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+))
+logger.addHandler(stream_handler)
+set_logger(logger)
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -32,18 +43,18 @@ login_button_xpath = '//button[@type="submit"]//div[contains(text(), "Einloggen"
 
 def _login(driver: WebDriver, wait: WebDriverWait) -> bool:
     try:
-        logging.info('Waiting for login button')
+        logger.info('Waiting for login button')
         wait.until(expected_conditions.element_to_be_clickable((By.XPATH, login_button_xpath)))
 
-        logging.info('Found login button, filling form data')
+        logger.info('Found login button, filling form data')
         driver.find_element(by=By.XPATH, value=email_address_input_xpath).send_keys(config['personio']['email_address'])
         driver.find_element(by=By.XPATH, value=password_input_xpath).send_keys(config['personio']['password'])
 
-        logging.info('Clicking login button')
+        logger.info('Clicking login button')
         driver.find_element(By.XPATH, login_button_xpath).click()
         return True
     except TimeoutException:
-        logging.info('No login button, skipping login page')
+        logger.info('No login button, skipping login page')
         return False
 
 
@@ -53,26 +64,26 @@ continue_login_button_xpath = '//button[@type="submit" and contains(text(), "Log
 
 def _token(driver: WebDriver, wait: WebDriverWait) -> bool:
     try:
-        logging.info('Waiting for continue login button')
+        logger.info('Waiting for continue login button')
         wait.until(expected_conditions.element_to_be_clickable((By.XPATH, continue_login_button_xpath)))
 
-        logging.warning('Found continue login button, waiting for you to enter the token')
+        logger.warning('Found continue login button, waiting for you to enter the token')
 
         while True:
             token_input = driver.find_element(by=By.XPATH, value=token_input_xpath)
             current_token = token_input.get_attribute('value')
-            logging.info(f'Current token: "{current_token}"')
+            logger.info(f'Current token: "{current_token}"')
             if len(current_token) > 0:
-                logging.warning('Token entered, continuing')
+                logger.warning('Token entered, continuing')
                 break
-            logging.warning('No token, sleeping')
+            logger.warning('No token, sleeping')
             time.sleep(1)
 
-        logging.info('Clicking continue login button')
+        logger.info('Clicking continue login button')
         driver.find_element(By.XPATH, continue_login_button_xpath).click()
         return True
     except TimeoutException:
-        logging.info('No continue login button, skipping token page')
+        logger.info('No continue login button, skipping token page')
         return False
 
 
@@ -80,10 +91,10 @@ attendance_button_xpath = '//a[@data-test-id="navsidebar-sub-myAttendance"]'
 
 
 def _attendance(driver: WebDriver, wait: WebDriverWait):
-    logging.info('Waiting for attendance button')
+    logger.info('Waiting for attendance button')
     wait.until(expected_conditions.element_to_be_clickable((By.XPATH, attendance_button_xpath)))
 
-    logging.info('Found attendance button, clicking')
+    logger.info('Found attendance button, clicking')
     driver.find_element(By.XPATH, attendance_button_xpath).click()
 
 
@@ -94,7 +105,7 @@ def do_stuff():
     with webdriver.Firefox(service=service, options=firefox_options) as driver:
         wait = WebDriverWait(driver, 5)
 
-        logging.info(f'Opening {domain=} ({subdomain=})')
+        logger.info(f'Opening {domain=} ({subdomain=})')
         driver.get(domain)
 
         if _login(driver, wait):
